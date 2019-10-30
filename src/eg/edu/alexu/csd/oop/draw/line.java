ddd3f67a65bcdb38326c137cd0ead158a8d381fe
unknown <mohamedsalaaa7@gmail.com>
2019-10-31 00:34:36 +0200

    Commit 2

---
+++ src/eg/edu/alexu/csd/oop/draw/line.java
@@ -0,0 +1,30 @@
+package eg.edu.alexu.csd.oop.draw;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class line extends dummyshape {
+    int endX, endY;
+
+    public line(int x, int y, Color color, int endX, int endY) {
+        super(x, y, color, "line");
+        this.endX = endX;
+        this.endY = endY;
+
+
+    }
+
+    @Override
+    public void draw(Graphics g) {
+        g.setColor(Color.black);
+        g.drawLine(this.x, this.y, endX, endY);
+    }
+
+    public void setEndX(int endX) {
+        this.endX = endX;
+    }
+
+    public void setEndY(int endY) {
+        this.endY = endY;
+    }
+}
